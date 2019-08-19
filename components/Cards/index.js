ae51c22951d503fe88c34e897fd77cc0870f0440
Kasit Nganthavee <kasit.nganthavee@gmail.com>
2019-08-19 09:00:10 +0700

    reached MVP with minor css display problems

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,60 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        // console.log(response.data)
+        response.data.articles['javascript'].forEach(article => {
+            let newCard = cardComponent(article.headline, article.authorPhoto, article.authorName)
+            articlesContainer.appendChild(newCard)
+        })
+        response.data.articles['bootstrap'].forEach(article => {
+            let newCard = cardComponent(article.headline, article.authorPhoto, article.authorName)
+            articlesContainer.appendChild(newCard)
+        })
+        response.data.articles['technology'].forEach(article => {
+            let newCard = cardComponent(article.headline, article.authorPhoto, article.authorName)
+            articlesContainer.appendChild(newCard)
+        })
+        response.data.articles['jquery'].forEach(article => {
+            let newCard = cardComponent(article.headline, article.authorPhoto, article.authorName)
+            articlesContainer.appendChild(newCard)
+        })
+        response.data.articles['node'].forEach(article => {
+            let newCard = cardComponent(article.headline, article.authorPhoto, article.authorName)
+            articlesContainer.appendChild(newCard)
+        })
+    })
+    .catch(error => {
+        console.error(error)
+    })
+
+let articlesContainer = document.querySelector('.cards-container')
+
+function cardComponent(headline, imgUrl, authorName) {
+    const cardDiv = document.createElement('div')
+    const headLineDiv = document.createElement('div')
+    const authorDiv = document.createElement('div')
+    const imgContainerDiv = document.createElement('div')
+    const imgContent = document.createElement('img')
+    const authorNameSpan = document.createElement('span')
+
+    cardDiv.classList.add('card')
+    headLineDiv.classList.add('headline')
+    authorDiv.classList.add('author')
+    imgContainerDiv.classList.add('img-container')
+
+    cardDiv.appendChild(headLineDiv)
+    cardDiv.appendChild(authorDiv)
+    authorDiv.appendChild(imgContainerDiv)
+    imgContainerDiv.appendChild(imgContent)
+    authorDiv.appendChild(authorNameSpan)
+
+    headLineDiv.textContent = headline
+    imgContent.src = imgUrl
+    authorNameSpan.textContent = `By: ${authorName}`
+
+    return cardDiv
+
+}
